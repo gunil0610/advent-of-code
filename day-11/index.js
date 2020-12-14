46b19a308f10a19e68545749d5ad4e3c2d2a0187
Gunil <gunil.na1@gmail.com>
2020-12-14 23:33:24 +0100

    day11 refactoring

--- day-11/index.js
+++ day-11/index.js
@@ -71,30 +71,7 @@ const lines = fs
 // };
 
 // pt2
-
-const checkRight = (rowIndex, index, arr) => {
-  const row = arr[rowIndex].split('');
-  if (row[index] && row[index] === '#') {
-    return 1;
-  } else if (row[index] && row[index] === 'L') {
-    return 0;
-  } else if (row[index] && row[index] === '.') {
-    return checkRight(rowIndex, index + 1, arr);
-  }
-  return 0;
-};
-const checkLeft = (rowIndex, index, arr) => {
-  const row = arr[rowIndex].split('');
-  if (row[index] && row[index] === '#') {
-    return 1;
-  } else if (row[index] && row[index] === 'L') {
-    return 0;
-  } else if (row[index] && row[index] === '.') {
-    return checkLeft(rowIndex, index - 1, arr);
-  }
-  return 0;
-};
-const checkUpRight = (rowIndex, index, arr) => {
+const check = (rowIndex, index, arr, increment) => {
   if (!arr[rowIndex]) {
     return 0;
   }
@@ -104,77 +81,7 @@ const checkUpRight = (rowIndex, index, arr) => {
   } else if (row[index] && row[index] === 'L') {
     return 0;
   } else if (row[index] && row[index] === '.') {
-    return checkUpRight(rowIndex - 1, index + 1, arr);
-  }
-  return 0;
-};
-const checkUp = (rowIndex, index, arr) => {
-  if (!arr[rowIndex]) {
-    return 0;
-  }
-  const row = arr[rowIndex].split('');
-  if (row[index] && row[index] === '#') {
-    return 1;
-  } else if (row[index] && row[index] === 'L') {
-    return 0;
-  } else if (row[index] && row[index] === '.') {
-    return checkUp(rowIndex - 1, index, arr);
-  }
-  return 0;
-};
-const checkUpLeft = (rowIndex, index, arr) => {
-  if (!arr[rowIndex]) {
-    return 0;
-  }
-  const row = arr[rowIndex].split('');
-  if (row[index] && row[index] === '#') {
-    return 1;
-  } else if (row[index] && row[index] === 'L') {
-    return 0;
-  } else if (row[index] && row[index] === '.') {
-    return checkUpLeft(rowIndex - 1, index - 1, arr);
-  }
-  return 0;
-};
-const checkDownRight = (rowIndex, index, arr) => {
-  if (!arr[rowIndex]) {
-    return 0;
-  }
-  const row = arr[rowIndex].split('');
-  if (row[index] && row[index] === '#') {
-    return 1;
-  } else if (row[index] && row[index] === 'L') {
-    return 0;
-  } else if (row[index] && row[index] === '.') {
-    return checkDownRight(rowIndex + 1, index + 1, arr);
-  }
-  return 0;
-};
-const checkDown = (rowIndex, index, arr) => {
-  if (!arr[rowIndex]) {
-    return 0;
-  }
-  const row = arr[rowIndex].split('');
-  if (row[index] && row[index] === '#') {
-    return 1;
-  } else if (row[index] && row[index] === 'L') {
-    return 0;
-  } else if (row[index] && row[index] === '.') {
-    return checkDown(rowIndex + 1, index, arr);
-  }
-  return 0;
-};
-const checkDownLeft = (rowIndex, index, arr) => {
-  if (!arr[rowIndex]) {
-    return 0;
-  }
-  const row = arr[rowIndex].split('');
-  if (row[index] && row[index] === '#') {
-    return 1;
-  } else if (row[index] && row[index] === 'L') {
-    return 0;
-  } else if (row[index] && row[index] === '.') {
-    return checkDownLeft(rowIndex + 1, index - 1, arr);
+    return check(rowIndex + increment.y, index + increment.x, arr, increment);
   }
   return 0;
 };
@@ -182,16 +89,20 @@ const checkDownLeft = (rowIndex, index, arr) => {
 const checkOccupied = (index, rowIndex, arr) => {
   const intIndex = parseInt(index);
   const rowIntIndex = parseInt(rowIndex);
-
-  let occupied =
-    checkRight(rowIntIndex, intIndex + 1, arr) +
-    checkLeft(rowIntIndex, intIndex - 1, arr) +
-    checkUp(rowIntIndex - 1, intIndex, arr) +
-    checkUpRight(rowIntIndex - 1, intIndex + 1, arr) +
-    checkUpLeft(rowIntIndex - 1, intIndex - 1, arr) +
-    checkDown(rowIntIndex + 1, intIndex, arr) +
-    checkDownLeft(rowIntIndex + 1, intIndex - 1, arr) +
-    checkDownRight(rowIntIndex + 1, intIndex + 1, arr);
+  const direction = [
+    { x: -1, y: 0 },
+    { x: 1, y: 0 },
+    { x: 1, y: -1 },
+    { x: 0, y: -1 },
+    { x: -1, y: -1 },
+    { x: 1, y: 1 },
+    { x: 0, y: 1 },
+    { x: -1, y: 1 },
+  ];
+  let occupied = 0;
+  for (const dir of direction) {
+    occupied += check(rowIntIndex + dir.y, intIndex + dir.x, arr, dir);
+  }
   return occupied;
 };
 
@@ -265,15 +176,3 @@ const printOccupiedNumber = (arr) => {
 
 const resArr = run(lines);
 printOccupiedNumber(resArr);
-// const arr1 = rule(lines, 1);
-// const arr2 = rule(arr1, 2);
-// const arr3 = rule(arr2, 1);
-// const arr4 = rule(arr3, 1);
-// const arr5 = rule(arr4, 1);
-// const arr6 = rule(arr5, 1);
-// console.log(arr1);
-// console.log(arr2);
-// console.log(arr3);
-// console.log(arr4);
-// console.log(arr5);
-// console.log(arr6);
