ab7ea21bd86a3300cb7d3ebd85615e15561e8e94
Gunil <gunil.na1@gmail.com>
2020-12-16 20:37:53 +0100

    day 13 pt 2

--- day-13/index.js
+++ day-13/index.js
@@ -1,4 +1,6 @@
+const { timeStamp, time } = require('console');
 const fs = require('fs');
+const { parse } = require('path');
 
 const input = fs
   .readFileSync('input.txt', { encoding: 'utf-8' })
@@ -7,14 +9,15 @@ const input = fs
   .split('\n')
   .filter((x) => x);
 
-const time = input[0];
-const buses = input[1]
-  .split('x')
-  .join('')
-  .split(',')
-  .filter((x) => x);
-
 // // pt1
+
+// const time = input[0];
+// const buses = input[1]
+//   .split('x')
+//   .join('')
+//   .split(',')
+//   .filter((x) => x);
+
 // const pt1 = (time, buses) => {
 //   let minTime = 1000;
 //   let earliestBus = 0;
@@ -29,3 +32,32 @@ const buses = input[1]
 // };
 
 // pt1(time, buses);
+
+// pt2
+const part2 = () => {
+  const pt2Input = input[1].split(',').filter((x) => x);
+
+  const buses = [];
+  pt2Input.forEach((bus, index) => {
+    if (bus !== 'x') {
+      buses.push([parseInt(bus), index]);
+    }
+  });
+
+  let step = buses[0][0];
+  let i = 0;
+
+  buses.slice(1).forEach(([bus, busIndex]) => {
+    while (true) {
+      if ((i + busIndex) % bus === 0) {
+        step *= bus;
+        break;
+      }
+      i += step;
+    }
+  });
+
+  console.log(i);
+};
+
+part2();
