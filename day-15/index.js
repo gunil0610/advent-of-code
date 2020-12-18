9dcb0811848d5ad7d9781cf29fee6015b00740f5
Gunil <gunil.na1@gmail.com>
2020-12-19 00:43:26 +0100

    day 15 pt1

--- day-15/index.js
+++ day-15/index.js
@@ -3,6 +3,29 @@ const fs = require('fs');
 const input = fs
   .readFileSync('input.txt', { encoding: 'utf-8' })
   .split(',')
-  .filter((x) => x);
+  .map((x) => parseInt(x));
 
-console.log(input);
+const obj = {};
+
+const part1 = (num, i) => {
+  // stop when i is 2020
+  if (i > 2019) {
+    return num;
+  }
+  let nextNum = 0;
+
+  if (input[i]) {
+    nextNum = input[i];
+    obj[num] = i;
+    return part1(nextNum, i + 1);
+  }
+
+  if (obj[num]) {
+    nextNum = i - obj[num];
+  }
+  obj[num] = i;
+  return part1(nextNum, i + 1);
+};
+
+const res = part1(input[0], 1);
+console.log(res);
