d7691dc4ff696f28e2bca7d442e6717ccccd4dd9
Gunil <gunil.na1@gmail.com>
2020-12-19 02:09:07 +0100

    day-15 pt2

--- day-15/index.js
+++ day-15/index.js
@@ -7,25 +7,45 @@ const input = fs
 
 const obj = {};
 
-const part1 = (num, i) => {
-  // stop when i is 2020
-  if (i > 2019) {
-    return num;
-  }
-  let nextNum = 0;
+// const part1 = (num, i) => {
+//   // stop when i is 2020
+//   if (i > 2019) {
+//     return num;
+//   }
+//   let nextNum = 0;
 
-  if (input[i]) {
-    nextNum = input[i];
-    obj[num] = i;
-    return part1(nextNum, i + 1);
-  }
+//   if (input[i]) {
+//     nextNum = input[i];
+//     obj[num] = i;
+//     console.log(nextNum, i);
+//     return part1(nextNum, i + 1);
+//   }
 
-  if (obj[num]) {
-    nextNum = i - obj[num];
+//   if (obj[num]) {
+//     nextNum = i - obj[num];
+//   }
+//   obj[num] = i;
+//   console.log(nextNum, i);
+//   return part1(nextNum, i + 1);
+// };
+
+// const res = part1(input[0], 1);
+// console.log(res);
+
+const solve = (input, limit) => {
+  let indexes = new Map(input.map((val, index) => [val, index + 1]));
+  let bucket = NaN;
+  let target = input[input.length - 1];
+  for (let index = input.length; index < limit; index++) {
+    target = indexes.has(target) ? index - indexes.get(target) : 0;
+    indexes.set(bucket, index);
+    bucket = target;
+    console.log(indexes);
   }
-  obj[num] = i;
-  return part1(nextNum, i + 1);
+  return target;
 };
 
-const res = part1(input[0], 1);
+const limit = 30000000;
+
+res = solve(input, limit);
 console.log(res);
