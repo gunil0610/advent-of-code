20ceeab028ec41ac288743cffad37ea2116ab6b4
Gunil <gunil.na1@gmail.com>
2020-12-26 16:44:41 +0100

    day18 pt2

--- day-18/index.js
+++ day-18/index.js
@@ -7,27 +7,56 @@ const input = fs
   .split('\n')
   .filter((x) => x);
 
-const solve = (string) => {
-  let tokens = string.split(' ');
+// pt 1
+// const solve = (string) => {
+//   let tokens = string.split(' ');
 
-  while (tokens.length > 1) {
-    tokens = [eval(tokens.slice(0, 3).join(''))].concat(tokens.slice(3));
-  }
-  return tokens[0];
+//   while (tokens.length > 1) {
+//     tokens = [eval(tokens.slice(0, 3).join(''))].concat(tokens.slice(3));
+//   }
+//   return tokens[0];
+// };
+
+// const solveWithParenthesis = (string) => {
+//   while (/\(/.test(string)) {
+//     string = string.replace(/\(([^()]+)\)/g, (match, group) => {
+//       return solve(group);
+//     });
+//   }
+//   return solve(string);
+// };
+
+// const res = input.reduce((acc, line) => {
+//   acc += solveWithParenthesis(line);
+//   return acc;
+// }, 0);
+
+// console.log(res);
+
+// pt2
+const solve2 = (string) => {
+  let tokens = string.split(' * ');
+
+  let newToken = tokens.map((item) => eval(item));
+  return eval(newToken.join('*'));
+  // while (tokens.length > 1) {
+  //   tokens = [eval(tokens[0].join(''))].concat(tokens.slice(3));
+  // }
+  // return tokens[0];
 };
 
-const solveWithParenthesis = (string) => {
+const solveWithParenthesis2 = (string) => {
   while (/\(/.test(string)) {
     string = string.replace(/\(([^()]+)\)/g, (match, group) => {
-      return solve(group);
+      return solve2(group);
     });
   }
-  return solve(string);
+  return solve2(string);
 };
 
-const res = input.reduce((acc, line) => {
-  acc += solveWithParenthesis(line);
+const res2 = input.reduce((acc, line) => {
+  acc += solveWithParenthesis2(line);
   return acc;
 }, 0);
 
-console.log(res);
+console.log(res2);
